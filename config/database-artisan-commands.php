9126a8e35c741bebef3c4453d24580e98bb7f04d
Rafael Lima <rafaelrglima@gmail.com>
2019-04-19 22:20:39 +0100

    sync remote to local almost done

---
+++ config/database-artisan-commands.php
@@ -0,0 +1,38 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | SSH Vhost config name
+    |--------------------------------------------------------------------------
+    |
+    | Here is the ssh config name defined inside ~/.ssh/config like below:
+    |
+    | Host remotesshserver
+    | HostName 99.99.99.99
+    | User root
+    | IdentityFile ~/.ssh/yoursshkey
+    |
+    */
+    'staging_server' => [
+        'ssh' => env('STG_SSH_HOSTNAME', 'remotesshserver'),
+        'docker' => env('STG_DOCKER_HOSTNAME', 'app-mysql'),
+        'database' => env('STG_DATABASE_NAME', 'appdb'),
+        'password' => env('STG_DATABASE_PW', 'appdb')
+    ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are extra database connections that can be used for local development
+    |
+    */
+    'local_server' => [
+        'hostname' => env('LOCAL_HOSTNAME', 'localhost'),
+        'username' => env('LOCAL_DATABASE_NAME', 'appdb'),
+        'password' => env('LOCAL_DATABASE_PW', 'appdb')
+    ],
+];
